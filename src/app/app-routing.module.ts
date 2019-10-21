53b0969374b409340cbe2aaf45fad0dc5ab3b62e
nuochong <nuo.chong@163.com>
2019-10-21 13:43:49 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticleComponent } from './pages/article/article.component';
+import { InitComponent } from './pages/init/init.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: InitComponent, data: { title: '文章详情' }
+  },
+  {
+    path: 'article', component: ArticleComponent, data: { title: '文章详情' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './pages/article/article.component';
+import { InitComponent } from './pages/init/init.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    InitComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
