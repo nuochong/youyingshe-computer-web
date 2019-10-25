5f782f0a158388af033140c9c947097bcb2c2b8c
nuochong <nuo.chong@163.com>
2019-10-25 16:34:08 +0800

    update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,13 +7,22 @@ import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular
 })
 export class HeaderComponent implements OnInit {
   expression: boolean = false;
+  expression2: boolean = false;
   @ViewChild('dropdownmenu',{static: false}) menu:ElementRef;
   @ViewChild('dropdown',{static: false}) dropdown:ElementRef;
+  @ViewChild('dropdownmenu2',{static: false}) menu2:ElementRef;
+  @ViewChild('dropdown2',{static: false}) dropdown2:ElementRef;
+  dayNight:boolean = false;
+  fontFamily:string = 'hei';
+  fontStyle:string = 'jian';
   constructor() { }
 
   ngOnInit() {
   }
-
+  changeDayNight(){
+    this.dayNight = !this.dayNight;
+    console.log('this.dayNight :', this.dayNight);
+  }
   dropdownMenu($event): void {
     $event.preventDefault();
     this.expression = !this.expression;
@@ -23,15 +32,24 @@ export class HeaderComponent implements OnInit {
     console.log('进入')
   }
   dropdownMenuHidden($event){
-    //this.expression = false;
-    console.log('离开',$event[0])
     console.log('离开',$event[0].toElement)
     console.log('this.menu :', this.menu.nativeElement);
     if($event[0].toElement != this.menu.nativeElement){
       console.log('相等了')
       this.expression = false;
     }
-
+  }
+  dropdownMenuOpen2(){
+    this.expression2 = true;
+    console.log('进入')
+  }
+  dropdownMenuHidden2($event){
+    console.log('离开',$event[0].toElement)
+    console.log('this.menu :', this.menu2.nativeElement);
+    if($event[0].toElement != this.menu2.nativeElement){
+      console.log('相等了')
+      this.expression2 = false;
+    }
   }
   dropdowns($event){
     if($event[0].toElement != this.dropdown.nativeElement){
@@ -41,8 +59,5 @@ export class HeaderComponent implements OnInit {
   }
   @HostListener('mouseleave', ['$event']) onMouseLeave(e) {
     // console.log('全局离开事件',e)
-    // const index = this.getActived();
-
-    // this.setActiveLine(index);
   }
 }
