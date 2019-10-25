64b7b7d6e8c0dc1b374ba0a64420867eef109581
nuochong <nuo.chong@163.com>
2019-10-25 16:49:56 +0800

    ate

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,18 +8,23 @@ import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular
 export class HeaderComponent implements OnInit {
   expression: boolean = false;
   expression2: boolean = false;
-  @ViewChild('dropdownmenu',{static: false}) menu:ElementRef;
-  @ViewChild('dropdown',{static: false}) dropdown:ElementRef;
-  @ViewChild('dropdownmenu2',{static: false}) menu2:ElementRef;
-  @ViewChild('dropdown2',{static: false}) dropdown2:ElementRef;
-  dayNight:boolean = false;
-  fontFamily:string = 'hei';
-  fontStyle:string = 'jian';
+  @ViewChild('dropdownmenu', { static: false }) menu: ElementRef;
+  @ViewChild('dropdown', { static: false }) dropdown: ElementRef;
+  @ViewChild('dropdownmenu2', { static: false }) menu2: ElementRef;
+  @ViewChild('dropdown2', { static: false }) dropdown2: ElementRef;
+  dayNight: boolean = false;
+  fontFamily: string = 'hei';
+  fontStyle: string = 'jian';
+  styleModal: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
-  changeDayNight(){
+  changeStyleModal() {
+    console.log('激活了')
+    this.styleModal = !this.styleModal;
+  }
+  changeDayNight() {
     this.dayNight = !this.dayNight;
     console.log('this.dayNight :', this.dayNight);
   }
@@ -27,32 +32,32 @@ export class HeaderComponent implements OnInit {
     $event.preventDefault();
     this.expression = !this.expression;
   }
-  dropdownMenuOpen(){
+  dropdownMenuOpen() {
     this.expression = true;
     console.log('进入')
   }
-  dropdownMenuHidden($event){
-    console.log('离开',$event[0].toElement)
+  dropdownMenuHidden($event) {
+    console.log('离开', $event[0].toElement)
     console.log('this.menu :', this.menu.nativeElement);
-    if($event[0].toElement != this.menu.nativeElement){
+    if ($event[0].toElement != this.menu.nativeElement) {
       console.log('相等了')
       this.expression = false;
     }
   }
-  dropdownMenuOpen2(){
+  dropdownMenuOpen2() {
     this.expression2 = true;
     console.log('进入')
   }
-  dropdownMenuHidden2($event){
-    console.log('离开',$event[0].toElement)
+  dropdownMenuHidden2($event) {
+    console.log('离开', $event[0].toElement)
     console.log('this.menu :', this.menu2.nativeElement);
-    if($event[0].toElement != this.menu2.nativeElement){
+    if ($event[0].toElement != this.menu2.nativeElement) {
       console.log('相等了')
       this.expression2 = false;
     }
   }
-  dropdowns($event){
-    if($event[0].toElement != this.dropdown.nativeElement){
+  dropdowns($event) {
+    if ($event[0].toElement != this.dropdown.nativeElement) {
       console.log('相等了')
       this.expression = false;
     }
