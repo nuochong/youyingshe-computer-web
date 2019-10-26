b4e9fbc844ef0e9f927cf062110835c17088a18a
nuochong <nuo.chong@163.com>
2019-10-26 19:09:17 +0800

    update

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
-
+  showDeleteBtn: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  changeShowDeleteBtn() {
+    this.showDeleteBtn = true;
+  }
 }
