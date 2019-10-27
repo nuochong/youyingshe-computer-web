d9dbb8469309558ff5844ec92253841f52733ea9
nuochong <nuo.chong@163.com>
2019-10-27 20:48:04 +0800

    update

--- src/app/pages/reset-mobile/reset-mobile.component.ts
+++ src/app/pages/reset-mobile/reset-mobile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-reset-mobile',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-mobile.component.scss']
 })
 export class ResetMobileComponent implements OnInit {
-
+  styleModal: boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  close($event) {
+    console.log('传递的数据', $event)
+    this.styleModal = !$event ? true : false;
+  }
+  changeStyleModal() {
+    console.log('激活了')
+    this.styleModal = !this.styleModal;
   }
 
 }
