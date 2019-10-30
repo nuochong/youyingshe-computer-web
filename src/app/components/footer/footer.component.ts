4090091ea16ce07db404e72c6545419921f192dd
nuochong <nuo.chong@163.com>
2019-10-30 20:48:29 +0800

    update

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  isVisible = false;
+  isConfirmLoading = false;
   constructor() { }
 
   ngOnInit() {
   }
+  showModal(): void {
+    this.isVisible = true;
+  }
 
+  handleOk(): void {
+    this.isConfirmLoading = true;
+    setTimeout(() => {
+      this.isVisible = false;
+      this.isConfirmLoading = false;
+    }, 3000);
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
 }
