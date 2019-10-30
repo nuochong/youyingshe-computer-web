b94d445a0e7760d2f3996469f7a562a474c5e0e9
nuochong <nuo.chong@163.com>
2019-10-30 15:48:16 +0800

    update

--- src/app/pages/write/write.component.ts
+++ src/app/pages/write/write.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EditorConfig } from '../../components/editor/model/editor-config';
+import { NzModalService, NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-write',
@@ -12,7 +13,7 @@ export class WriteComponent implements OnInit {
   conf = new EditorConfig();
   markdown = '测试语句';
 
-  constructor() { }
+  constructor(private modalService: NzModalService,private message: NzMessageService) { }
 
   ngOnInit() {
   }
@@ -24,6 +25,22 @@ export class WriteComponent implements OnInit {
   //新建文集
   changeNewFile(){
     this.showNewFile = !this.showNewFile;
+  }
+  showDeleteConfirm(): void {
+    this.modalService.confirm({
+      nzTitle: '您确定要删除此任务吗?',
+      nzContent: '确认删除文章《😆》，文章将被移动到回收站，您可以在那里恢复它。60天后将被彻底删除。',
+      nzOkText: '确认',
+      nzOkType: 'danger',
+      nzOnOk: () => console.log('OK'),
+      nzCancelText: '取消',
+      nzOnCancel: () => console.log('Cancel')
+    });
+  }
+
+  //全局提示
+  createMessage(type: string): void {
+    this.message.create(type, `This is a message of ${type}`);
   }
 
 }
